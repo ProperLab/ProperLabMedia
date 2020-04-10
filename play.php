2d814db09cc4b5677de54c0edc89e742f2968e27
ProperCloud <contact.propercloud@gmail.com>
2020-04-10 21:38:56 +0200

    Sidebar

--- play.php
+++ play.php
@@ -13,10 +13,30 @@
     <link rel="icon" href="/assets/img/icon/favicon.ico">
     <link href="/assets/vendor/bootsrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="/assets/css/main.css" rel="stylesheet">
+    <link href="/assets/css/simple-sidebar.css" rel="stylesheet">
 
 </head>
 
 <body class="text-center" onload="$('#startSesion').modal('show')">
+
+<div class="d-flex" id="wrapper">
+
+<!-- Sidebar -->
+<div id="sidebar-wrapper">
+  <div class="sidebar-heading">Amigos</div>
+  <div class="list-group list-group-flush">
+    <a href="#" class="list-group-item list-group-item-action bg-white">Amigo 1</a>
+    <a href="#" class="list-group-item list-group-item-action bg-white">Amigo 2</a>
+    <a href="#" class="list-group-item list-group-item-action bg-white">Amigo 3</a>
+    <a href="#" class="list-group-item list-group-item-action bg-white">Amigo 4</a>
+    <a href="#" class="list-group-item list-group-item-action bg-white">Amigo 5</a>
+    <a href="#" class="list-group-item list-group-item-action bg-white">Amigo 6</a>
+  </div>
+</div>
+<!-- /#sidebar-wrapper -->
+
+</div>
+<!-- /#wrapper -->
 
     <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
         <header class="masthead mb-auto">
@@ -32,9 +52,15 @@
         <main role="main" class="inner cover">
             <h1 class="cover-heading">ProperLab Media</h1>
             <p class="lead">Un reproductor multimedia minimalista online para ver series o películas con amigos.</p>
+            <p class="lead">¡Espera a tus amigos o dale a ProperLab para comenzar!</p>
+            <!-- Page Content -->
+            <div id="page-content-wrapper">
+            <button class="btn btn-primary" id="menu-toggle">Menu de amigos</button>
+            </div>
+            <!-- /#page-content-wrapper -->
             <p class="lead">
                 <?php
-                echo '<video src="https://propercloud.sytes.net/assets/video/code-lyoko.mp4" controls playsinline="true" preload="true" poster="poster.jpg" width="auto" height="auto">Tu navegador no soporta la etiqueta video.</video>';
+                echo '<video src="https://propercloud.sytes.net/assets/video/code-lyoko.mp4" controls playsinline="true" preload="true" poster="/assets/img/icon/favicon.ico" width="auto" height="auto">Tu navegador no soporta la etiqueta video.</video>';
                 ?>
             </p>
         </main>
@@ -69,6 +95,16 @@
     <script src="/assets/vendor/js/jquery-3.4.1.min.js"></script>
     <script src="/assets/vendor/js/popper.min.js"></script>
     <script src="/assets/vendor/bootsrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/js/bootstrap.bundle.js"></script>
+    <script src="/assets/js/gulpfile.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+    });
+    </script>
 
 </body>
 
