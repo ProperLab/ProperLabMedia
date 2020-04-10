899a8b8381af997144243517e680293915024557
MakerLab-Dev <contact.makerlab@gmail.com>
2020-04-10 21:12:24 +0200

    fix gitignore

---
+++ api/dbconf.example.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * ProperLabMedia\DbConf
+ * © 2020 ProperLab - All Rights Reserved
+ */
+//DATABASE CONNECTION VARIABLES
+$host = 'localhost'; // Host name
+$username = 'root'; // Mysql username
+$password = 'ProperLab'; // Mysql password
+$db_name = 'plm'; // Database name
+
+$tbl_rooms = 'salas';
