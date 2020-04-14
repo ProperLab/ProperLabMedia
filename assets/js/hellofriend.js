998be8ea9e6fd054c45eefb7b55cd65302baaf91
ProperCloud <contact.propercloud@gmail.com>
2020-04-14 20:00:04 +0200

    Hello friend (Mr Robot)

--- assets/js/hellofriend.js
+++ assets/js/hellofriend.js
@@ -1,7 +1,23 @@
 function fetchFriends() {
-var fetchFriendsInterval = setInterval(function() {
+  var fetchFriendsInterval = setInterval(function () {
     var now = new Date().getTime();
 
-    // Obtener datos
+    $.post(
+      "/api/playAPI.php",
+      {
+        action: "fetchUsers",
+        salaId: document.getElementById("salaId").value,
+      },
+      function (data, status) {}
+    )
+      .done(function (data) {
+        var parsed = JSON.parse(data);
+        parsed.forEach(element => {
+            console.log(element);
+        });
+      })
+      .fail(function (data) {
+        alert("Ha ocurrido un error: " + data.responseText);
+      });
   }, 500);
 }
